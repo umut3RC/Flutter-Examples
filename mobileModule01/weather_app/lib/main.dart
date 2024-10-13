2c61a6e4e55d364cdac4db86564a5dd9cb4be455
umut3RC <maeumut@gmail.com>
2024-10-13 17:16:30 +0300

    m01-ex00 finished

--- mobileModule01/weather_app/lib/main.dart
+++ mobileModule01/weather_app/lib/main.dart
@@ -1,40 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: ''),
+      title: 'Tab Example',
+      home: TabScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class TabScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TabScreenState createState() => _TabScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  final List<String> _titles = ["Currently", "Today", "Weekly"];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                Center(child: Text('Currently', style: TextStyle(fontSize: 24))),
+                Center(child: Text('Today', style: TextStyle(fontSize: 24))),
+                Center(child: Text('Weekly', style: TextStyle(fontSize: 24))),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(icon: Icon(Icons.sunny), text: "Currently"),
+            Tab(icon: Icon(Icons.calendar_today), text: "Today"),
+            Tab(icon: Icon(Icons.calendar_month), text: "Weekly"),
+          ],
+          labelStyle: const TextStyle(fontSize: 12),
+        ),
+      ),
     );
-    }
   }
 }

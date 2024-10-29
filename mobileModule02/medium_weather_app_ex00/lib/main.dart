3fbb0db927d908f9ab0f78dae0b84ea8fe0a62d1
umut3RC <maeumut@gmail.com>
2024-10-29 19:03:52 +0300

    m02-ex00

--- mobileModule02/medium_weather_app_ex00/lib/main.dart
+++ mobileModule02/medium_weather_app_ex00/lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tab Example',
+      title: 'Weather App',
       home: TabScreen(),
     );
   }
@@ -20,20 +20,29 @@ class TabScreen extends StatefulWidget {
   _TabScreenState createState() => _TabScreenState();
 }
 
-class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMixin {
+class _TabScreenState extends State<TabScreen>
+    with SingleTickerProviderStateMixin {
   TabController? _tabController;
-  String  _locationData = ' ';
+  TextEditingController? _Fieldcontroller;
   String query = '';
+
+  String _locationMessage = "Konum bilgisi alınamadı";
+  final String _denidedText =
+      "Geolocation is not available, please enable it in your App settings";
+  bool _isOnline = true;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
+    _Fieldcontroller = TextEditingController();
+    _determinePosition();
   }
 
   @override
   void dispose() {
     _tabController?.dispose();
+    _Fieldcontroller?.dispose();
     super.dispose();
   }
 
@@ -42,65 +51,160 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
       query = newQuery;
     });
   }
+
   void onEntered(String value) {
     setState(() {
-      _locationData = value;
+      _locationMessage = value;
     });
+  }
+
+  void ClearField() {
+    setState(() {
+      query = '';
+      _Fieldcontroller?.text = '';
+    });
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Konum servisinin etkin olup olmadığını kontrol edin
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _locationMessage = "Konum servisleri etkin değil.";
+        _isOnline = false;
+      });
+      return;
+    }
+
+    // Konum izni kontrol et
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _locationMessage = "Konum izni reddedildi.";
+          _isOnline = false;
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _locationMessage = "Konum izni kalıcı olarak reddedildi.";
+        _isOnline = false;
+      });
+      return;
+    }
+
+    // Konumu al
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _locationMessage = "${position.latitude} ${position.longitude}";
+    });
+  }
+
+  Widget CurrentTabObject() {
+    return (Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Currently', style: TextStyle(fontSize: 24)),
+          if (_isOnline)
+            Text(_locationMessage, style: const TextStyle(fontSize: 24))
+          else
+            Text(_denidedText,
+                textAlign: TextAlign.center, style: const TextStyle(fontSize: 24, color: Colors.red))
+        ],
+      ),
+    ));
+  }
+
+  Widget TodayTabObject() {
+    return (Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Today', style: TextStyle(fontSize: 24)),
+          if (_isOnline)
+            Text(_locationMessage, style: const TextStyle(fontSize: 24))
+          else
+            Text(_denidedText,
+                textAlign: TextAlign.center, style: const TextStyle(fontSize: 24, color: Colors.red))
+        ],
+      ),
+    ));
+  }
+
+  Widget WeeklyTabObject() {
+    return (Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Weekly', style: TextStyle(fontSize: 24)),
+          if (_isOnline)
+            Text(_locationMessage, style: const TextStyle(fontSize: 24))
+          else
+            Text(_denidedText,
+                textAlign: TextAlign.center, style: const TextStyle(fontSize: 24, color: Colors.red))
+        ],
+      ),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                onChanged: onQueryChanged,
+                controller: _Fieldcontroller,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  hintText: 'Search Location',
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () {
+                      ClearField();
+                    },
+                  ),
+                ),
+                onSubmitted: onEntered,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue[800],
+                  borderRadius:
+                      const BorderRadius.only(topRight: Radius.circular(10))),
+              child: IconButton(
+                  icon: const Icon(
+                    Icons.arrow_outward_sharp,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    onEntered(query);
+                  }),
+            )
+          ],
+        ),
       ),
       body: Column(
         children: [
-          Column(
-            children: [
-              Container(
-                child: TextField(
-                  onChanged: onQueryChanged,
-                  decoration: InputDecoration(
-                    labelText: 'Search',
-                    prefixIcon: Icon(Icons.search),
-                  ),
-                  onSubmitted: onEntered,
-                ),
-              ),
-            ],
-          ),
           Expanded(
             child: TabBarView(
               controller: _tabController,
               children: [
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Currently', style: TextStyle(fontSize: 24)),
-                      Text(_locationData, style: const TextStyle(fontSize: 24)),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Today', style: TextStyle(fontSize: 24)),
-                      Text(_locationData, style: const TextStyle(fontSize: 24)),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Weekly', style: TextStyle(fontSize: 24)),
-                      Text(_locationData, style: const TextStyle(fontSize: 24)),
-                    ],
-                  ),
-                ),
+                CurrentTabObject(),
+                TodayTabObject(),
+                WeeklyTabObject(),
               ],
             ),
           ),
@@ -121,110 +225,99 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
   }
 }
 
-
-
-
-
-/*
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  Position? _position;
-
-  void _getCurrentLocation() async {
-    Position position = await _determinePosition();
-    setState(() {
-      _position = position;
-    });
-  }
-
-  Future<Position> _determinePosition() async {
-    LocationPermission permission;
-
-    permission = await Geolocator.checkPermission();
-
-    if(permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if(permission == LocationPermission.denied) {
-        return Future.error('Location Permissions are denied');
-      }
-    }
-
-    return await Geolocator.getCurrentPosition();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Geolocation App"),
-      ),
-      body: Center(
-        child: _position != null ? Text('Current Location: ' + _position.toString()) : Text('No Location Data'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getCurrentLocation,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Konum Uygulaması',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const LocationScreen(),
+//     );
+//   }
+// }
+//
+// class LocationScreen extends StatefulWidget {
+//   const LocationScreen({super.key});
+//
+//   @override
+//   State<LocationScreen> createState() => _LocationScreenState();
+// }
+//
+// class _LocationScreenState extends State<LocationScreen> {
+//   String _locationMessage = "Konum bilgisi alınamadı";
+//   final String _denidedText = "Geolocation is not available, please enable it in your App settings";
+//   bool  _isOnline = true;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _determinePosition();
+//   }
+//
+//   Future<void> _determinePosition() async {
+//     bool serviceEnabled;
+//     LocationPermission permission;
+//
+//     // Konum servisinin etkin olup olmadığını kontrol edin
+//     serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//     if (!serviceEnabled) {
+//       setState(() {
+//         _locationMessage = "Konum servisleri etkin değil.";
+//         _isOnline = false;
+//       });
+//       return;
+//     }
+//
+//     // Konum izni kontrol et
+//     permission = await Geolocator.checkPermission();
+//     if (permission == LocationPermission.denied) {
+//       permission = await Geolocator.requestPermission();
+//       if (permission == LocationPermission.denied) {
+//         setState(() {
+//           _locationMessage = "Konum izni reddedildi.";
+//           _isOnline = false;
+//         });
+//         return;
+//       }
+//     }
+//
+//     if (permission == LocationPermission.deniedForever) {
+//       setState(() {
+//         _locationMessage = "Konum izni kalıcı olarak reddedildi.";
+//         _isOnline = false;
+//       });
+//       return;
+//     }
+//
+//     // Konumu al
+//     Position position = await Geolocator.getCurrentPosition();
+//     setState(() {
+//       _locationMessage =
+//       "Enlem: ${position.latitude}, Boylam: ${position.longitude}";
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Konum Uygulaması"),
+//       ),
+//       body: Center(
+//         child: Text(
+//           _locationMessage,
+//           style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//           textAlign: TextAlign.center,
+//         ),
+//       ),
+//     );
+//   }
+// }

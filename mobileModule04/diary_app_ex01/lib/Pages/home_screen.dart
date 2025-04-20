781c2d0d9abe0055318e7698682c3b522f75ac6b
umut3RC <maeumut@gmail.com>
2025-04-20 16:43:51 +0300

    fixing m04-ex01

--- mobileModule04/diary_app_ex01/lib/Pages/home_screen.dart
+++ mobileModule04/diary_app_ex01/lib/Pages/home_screen.dart
@@ -13,31 +13,55 @@ class _HomeScreenState extends State<HomeScreen> {
   final AuthService _authService = AuthService();
   int _lastSentimentIndex = 0;
   String _lastSentimentText = 'Happy';
+  List<Map<String, dynamic>> _userNotes = [];
 
-  Widget CreateEntryObj() {
-    return (Container(
-      alignment: Alignment.center,
+  Future<List<Map<String, dynamic>>> fetchUserNotes() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser == null) {
+      print("Kullanıcı giriş yapmamış.");
+      return [];
+    }
+
+    final String userEmail = currentUser.email!;
+
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance
+            .collection('notes')
+            .where('usermail', isEqualTo: userEmail)
+            .orderBy('date', descending: true) // İsteğe bağlı sıralama
+            .get();
+
+    // Verileri map'e dönüştür
+    return snapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+  }
+
+  Future<void> getAllEntries() async {
+    final notes = await fetchUserNotes();
+    setState(() {
+      _userNotes = notes;
+    });
+  }
+
+  void RefreshEntries() {
+    getAllEntries();
+  }
+  Widget CreateEntryObj(String title, String etime, int icn) {
+    return (ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.cyan[100]),
+      ),
+      onPressed: () {
+        print("büttürgeç");
+      },
       child: Column(
         children: [
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.cyan[100]),
-            ),
-            onPressed: () {
-              print("büttürgeç");
-            },
-            child: Column(
-              children: [
-                Text("date April 1, 2025 at 11:11:11"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.sentiment_satisfied),
-                    Text("Test Title"),
-                  ],
-                ),
-              ],
-            ),
+          Text(etime.toString()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [GetSentimentIcon(icn), Text(title)],
           ),
         ],
       ),
@@ -50,17 +74,10 @@ class _HomeScreenState extends State<HomeScreen> {
     );
     await usersRef.add({
       'date': DateTime.now(),
-      'icon': 0,
+      'icon': icn,
       'text': text,
       'title': title,
       'usermail': FirebaseAuth.instance.currentUser?.email,
-    });
-  }
-
-  void setIconSentiment(int status) {
-    setState(() {
-      _lastSentimentIndex = status;
-      _lastSentimentText = GetSentimentText(status);
     });
   }
 
@@ -74,6 +91,19 @@ class _HomeScreenState extends State<HomeScreen> {
         return "Unhappy";
       default:
         return ("Happy");
+    }
+  }
+
+  Widget GetSentimentIcon(int newStatus) {
+    switch (newStatus) {
+      case 0:
+        return Icon(Icons.sentiment_very_satisfied);
+      case 1:
+        return Icon(Icons.sentiment_satisfied);
+      case 2:
+        return Icon(Icons.sentiment_dissatisfied);
+      default:
+        return (Icon(Icons.sentiment_very_satisfied));
     }
   }
 
@@ -109,108 +139,115 @@ class _HomeScreenState extends State<HomeScreen> {
               style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all(Colors.cyanAccent),
               ),
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true,
-                    builder: (context) {
-                      final titleController = TextEditingController();
-                      final contentController = TextEditingController();
-                      int tempSentimentIndex = _lastSentimentIndex;
-                      String tempSentimentText = _lastSentimentText;
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  builder: (context) {
+                    final titleController = TextEditingController();
+                    final contentController = TextEditingController();
+                    // int tempSentimentIndex = _lastSentimentIndex;
+                    // String tempSentimentText = _lastSentimentText;
 
-                      return StatefulBuilder(
-                        builder: (BuildContext context, StateSetter setModalState) {
-                          void setIconSentiment(int status) {
-                            setModalState(() {
-                              tempSentimentIndex = status;
-                              tempSentimentText = GetSentimentText(status);
-                            });
-                          }
+                    return StatefulBuilder(
+                      builder: (
+                        BuildContext context,
+                        StateSetter setModalState,
+                      ) {
+                        void setIconSentiment(int status) {
+                          setModalState(() {
+                            _lastSentimentIndex = status;
+                            _lastSentimentText = GetSentimentText(status);
+                            print(_lastSentimentIndex);
+                          });
+                        }
 
-                          return Padding(
-                            padding: EdgeInsets.only(
-                              bottom: MediaQuery.of(context).viewInsets.bottom,
-                              left: 16,
-                              right: 16,
-                              top: 16,
-                            ),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                TextField(
-                                  controller: titleController,
-                                  decoration: InputDecoration(labelText: "Title"),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    IconButton(
-                                      onPressed: () => setIconSentiment(0),
-                                      icon: Icon(Icons.sentiment_very_satisfied),
-                                    ),
-                                    IconButton(
-                                      onPressed: () => setIconSentiment(1),
-                                      icon: Icon(Icons.sentiment_satisfied),
-                                    ),
-                                    IconButton(
-                                      onPressed: () => setIconSentiment(2),
-                                      icon: Icon(Icons.sentiment_dissatisfied),
-                                    ),
-                                  ],
-                                ),
-                                Text(tempSentimentText), // Güncellenen text buraya
-                                TextField(
-                                  controller: contentController,
-                                  decoration: InputDecoration(labelText: "Text"),
-                                  maxLines: 10,
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    if (titleController.text.isNotEmpty &&
-                                        contentController.text.isNotEmpty) {
-                                      PushNewEntryData(
-                                        titleController.text,
-                                        tempSentimentIndex,
-                                        contentController.text,
-                                      );
-                                      Navigator.pop(context);
-                                    }
-                                  },
-                                  child: Text("Add"),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  );
-                },
-                child: Text("Create an entry"),
+                        return Padding(
+                          padding: EdgeInsets.only(
+                            bottom: MediaQuery.of(context).viewInsets.bottom,
+                            left: 16,
+                            right: 16,
+                            top: 16,
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextField(
+                                controller: titleController,
+                                decoration: InputDecoration(labelText: "Title"),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    onPressed: () => setIconSentiment(0),
+                                    icon: Icon(Icons.sentiment_very_satisfied),
+                                  ),
+                                  IconButton(
+                                    onPressed: () => setIconSentiment(1),
+                                    icon: Icon(Icons.sentiment_satisfied),
+                                  ),
+                                  IconButton(
+                                    onPressed: () => setIconSentiment(2),
+                                    icon: Icon(Icons.sentiment_dissatisfied),
+                                  ),
+                                ],
+                              ),
+                              Text(_lastSentimentText),
+                              // Güncellenen text buraya
+                              TextField(
+                                controller: contentController,
+                                decoration: InputDecoration(labelText: "Text"),
+                                maxLines: 10,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (titleController.text.isNotEmpty &&
+                                      contentController.text.isNotEmpty) {
+                                    PushNewEntryData(
+                                      titleController.text,
+                                      _lastSentimentIndex,
+                                      contentController.text,
+                                    );
+                                    RefreshEntries();
+                                    Navigator.pop(context);
+                                  }
+                                },
+                                child: Text("Add"),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                );
+              },
+              child: Text("Create an entry"),
+            ),
+            TextButton(
+              onPressed: () {
+                getAllEntries();
+              },
+              child: Text('Refresh'),
             ),
             SizedBox(height: 20.0),
             Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  //Tüm günlük girdileri al sırala
-                  children: [
-                    CreateEntryObj(),
-                    SizedBox(height: 20.0),
-                    CreateEntryObj(),
-                    SizedBox(height: 20.0),
-                    CreateEntryObj(),
-                    SizedBox(height: 20.0),
-                    CreateEntryObj(),
-                    SizedBox(height: 20.0),
-                    CreateEntryObj(),
-                    SizedBox(height: 20.0),
-                    CreateEntryObj(),
-                  ],
-                ),
-              ),
+              child:
+                  _userNotes.isNotEmpty
+                      ? ListView.builder(
+                        itemCount: _userNotes.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            title: CreateEntryObj(_userNotes[index]['title'].toString(),_userNotes[index]['date'].toString(),int.parse(_userNotes[index]['icon'].toString())),
+                          );
+                        },
+                      )
+                      : Center(child: Text('Hiç not yok')),
             ),
+
+            // ),
           ],
         ),
       ),
